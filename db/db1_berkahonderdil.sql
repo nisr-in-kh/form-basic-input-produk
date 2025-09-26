3553d0479ea205cf22437cbce8d33732f9ccf395
nisr-in-kh <204346333+nisr-in-kh@users.noreply.github.com>
2025-09-26 17:08:46 +0700

    Perbaruan database

---
+++ db/db1_berkahonderdil.sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307:3307
+-- Generation Time: Sep 26, 2025 at 12:19 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db1_berkahonderdil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(100) NOT NULL,
+  `pass_admin` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama_admin`, `pass_admin`, `created_at`) VALUES
+(31, '', '', '2025-09-25 16:47:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `id_kel`
+--
+
+CREATE TABLE `id_kel` (
+  `id_kel` varchar(10) NOT NULL,
+  `id_kec` varchar(6) NOT NULL,
+  `nama_kel` varchar(100) NOT NULL,
+  `kode_pos` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kab`
+--
+
+CREATE TABLE `kab` (
+  `id_kab` varchar(4) NOT NULL,
+  `id_provinsi` varchar(2) NOT NULL,
+  `nama_kab` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kec`
+--
+
+CREATE TABLE `kec` (
+  `id_kec` varchar(6) NOT NULL,
+  `id_kab` varchar(4) NOT NULL,
+  `nama_kec` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `model`
+--
+
+CREATE TABLE `model` (
+  `id_mobil` int(11) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  `tahun` year(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provinsi`
+--
+
+CREATE TABLE `provinsi` (
+  `id_provinsi` varchar(2) NOT NULL,
+  `nama_provinsi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `id_req` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id_user` int(11) NOT NULL,
+  `brand_req` varchar(50) NOT NULL,
+  `model_req` varchar(100) NOT NULL,
+  `tahun_req` year(4) NOT NULL,
+  `sparepart_req` varchar(100) NOT NULL,
+  `ket` text DEFAULT NULL,
+  `status` enum('Menunggu','Diproses','Selesai') NOT NULL DEFAULT 'Menunggu'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `id_review` int(11) NOT NULL,
+  `id_sparepart` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `bintang` tinyint(5) NOT NULL,
+  `komentar` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sparepart`
+--
+
+CREATE TABLE `sparepart` (
+  `id_sparepart` int(11) NOT NULL,
+  `id_mobil` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `grade` enum('A','B','C') NOT NULL,
+  `harga` decimal(12,2) NOT NULL,
+  `deskripsi` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sparepart`
+--
+
+INSERT INTO `sparepart` (`id_sparepart`, `id_mobil`, `nama`, `stock`, `grade`, `harga`, `deskripsi`) VALUES
+(32, 0, 'Knalpot', 0, 'A', 100.00, 'Brumm...');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `id_provinsi` varchar(2) NOT NULL,
+  `id_kota` varchar(4) NOT NULL,
+  `id_kec` varchar(6) NOT NULL,
+  `id_kel` varchar(10) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `id_kel`
+--
+ALTER TABLE `id_kel`
+  ADD PRIMARY KEY (`id_kel`);
+
+--
+-- Indexes for table `kab`
+--
+ALTER TABLE `kab`
+  ADD PRIMARY KEY (`id_kab`);
+
+--
+-- Indexes for table `kec`
+--
+ALTER TABLE `kec`
+  ADD PRIMARY KEY (`id_kec`);
+
+--
+-- Indexes for table `model`
+--
+ALTER TABLE `model`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indexes for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  ADD PRIMARY KEY (`id_provinsi`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id_req`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id_review`);
+
+--
+-- Indexes for table `sparepart`
+--
+ALTER TABLE `sparepart`
+  ADD PRIMARY KEY (`id_sparepart`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `model`
+--
+ALTER TABLE `model`
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `id_req` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sparepart`
+--
+ALTER TABLE `sparepart`
+  MODIFY `id_sparepart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
